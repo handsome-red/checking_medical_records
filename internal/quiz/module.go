28df47090029eac602ab4a5b7341ad37002451de
Andrey <egorovandrey314@gmail.com>
2026-06-08 22:07:33 +0300

    backup

--- internal/quiz/module.go
+++ internal/quiz/module.go
@@ -3,18 +3,18 @@ package quiz
 
 import (
 	"embed"
+	"encoding/json"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi/v5"
 
 	"med_book/internal/quiz/handlers"
+	"med_book/internal/quiz/models"
 	"med_book/internal/quiz/repository"
 	"med_book/internal/quiz/service"
 )
-
-//go:embed questions.json
-var questionsJSON []byte
 
 //go:embed templates/*.html
 var templatesFS embed.FS
@@ -40,12 +40,29 @@ func (m *Module) Router() chi.Router {
 }
 
 func (m *Module) setupRoutes() {
+	// Загрузка вопросов из файла
+	questions, err := loadQuestionsFromFile("pkg/questions/questions.json")
+	if err != nil {
+		log.Fatalf("can not load questions: %v", err)
+	}
+
+	// Преобразование в JSON для совместимости с существующим сервисом
+	questionsJSON, err := json.Marshal(questions)
+	if err != nil {
+		log.Fatalf("can not marshal questions: %v", err)
+	}
+
 	// Инициализация квиза
 	store, err := repository.NewSQLiteStore("asd")
 	if err != nil {
-		log.Fatalf("can not create repository")
+		log.Fatalf("can not create repository: %v", err)
 	}
-	svc, _ := service.NewQuizService(store, questionsJSON)
+	
+	svc, err := service.NewQuizService(store, questionsJSON)
+	if err != nil {
+		log.Fatalf("can not create quiz service: %v", err)
+	}
+	
 	handler := handlers.NewWebHandler(svc, templatesFS)
 
 	// Статика квиза
@@ -60,4 +77,19 @@ func (m *Module) setupRoutes() {
 	m.router.Post("/submit", handler.Submit)     // /quiz/submit
 	m.router.Get("/result/{id}", handler.Result) // /quiz/result/123
 	m.router.Get("/download", handler.Download)  // /quiz/download
+}
+
+// loadQuestionsFromFile загружает вопросы из JSON файла
+func loadQuestionsFromFile(filePath string) ([]models.Question, error) {
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		return nil, err
+	}
+
+	var questions []models.Question
+	if err := json.Unmarshal(data, &questions); err != nil {
+		return nil, err
+	}
+
+	return questions, nil
 }
